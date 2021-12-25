68b3453bf552fb19b75ec34d6e007320e27494e3
Anubhav Gupta <mail.anubhav06@gmail.com>
2021-12-25 16:34:07 +0530

    Tracking Page

--- frontend/src/pages/TrackingPage.js
+++ frontend/src/pages/TrackingPage.js
@@ -2,39 +2,43 @@ import React, {useState, useEffect, useContext} from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import AuthContext from '../context/AuthContext'
 import { useHistory } from 'react-router-dom'
-
+import {
+    AddItemPopUp,
+    Heading,
+    HeadingPopUp,
+    InputGift,
+    InputPrice,
+    InputPerson,
+    InputLink,
+    ActualList,
+    ListObject,
+    GiftName,
+    GiftPrice,
+    ForWhom,
+    LinkToGift,
+    Delete,
+    AddItemButton,
+    AddToListButton,
+    Cross,
+  } from "../StyledElements/ListPageElements";
 
 const TrackingPage = () => {
-    let [person, setPerson] = useState([])
     let [trackingData, setTrackingData] = useState([])
-    let {authTokens} = useContext(AuthContext)
+    let {authTokens, user} = useContext(AuthContext)
 
     const history = useHistory()
+
+    
+    const [addItem, setAddItem] = useState(false);
+    const [items, setItems] = useState([]);
+    const [description, setNameOfGift] = useState("");
+    const [trackingID, setTrackingID] = useState(0);
+    const [recipient, setForWhomGift] = useState("");
+    const [company, setDeliveryCompany] = useState("");
+
+
     
     useEffect(()=> {
-
-        // To get the person (list of recipient) added by the user
-        let getPerson = async() =>{
-            let response = await fetch('https://gift-space.herokuapp.com/api/get-person/', {
-                method:'GET',
-                headers:{
-                    'Content-Type':'application/json',
-                    'Authorization':'Bearer ' + String(authTokens.access)
-                }
-            })
-            let data = await response.json()
-
-            if(response.status === 200){
-                // Store the data in useState
-                setPerson(data)
-                console.log('PERSON: ', data)
-            }
-            else{
-                alert('ERROR GETTING THE PERSON LIST: ', data)
-            }
-            
-        }
-
 
         // To get the tracking data
         let getTrackingData = async() =>{
@@ -50,6 +54,7 @@ const TrackingPage = () => {
             if(response.status === 200){
                 // Store the data in useState
                 setTrackingData(data)
+                setItems(data)
                 console.log('TRACKING DATA: ', data)
             }
             else{
@@ -57,12 +62,9 @@ const TrackingPage = () => {
             }
             
         }
-        
-
-       
+          
 
         // Call the function every time the page loads
-        getPerson()
         getTrackingData()
     }, [])
 
@@ -71,17 +73,126 @@ const TrackingPage = () => {
 
     return (
         <div>
-            <p>TRACKING PAGE! </p><br/><br/>
+            <p> ------ TRACKING PAGE!------ ADD BIG HEADING/LOGO HERE ------ </p><br/><br/>
 
-            <Link to='/add-tracking'> Add Tracking Number </Link>
-            {trackingData.map(data => (
-                <div key={data.id}>
-                    Company: {data.company} <br/>
-                    Tracking ID: {data.trackingID} <br/>
-                    Description: {data.description} <br/>
-                    Recipient: {data.recipient} <br/><br/>
-                </div>
-            ))}
+
+            <Heading>Add tracking data to your list</Heading>
+                
+                <AddItemPopUp show={addItem}>
+                    <Cross onClick={() => setAddItem(!addItem)} />
+                    <HeadingPopUp> ADD TRACKING DETAILS </HeadingPopUp>
+                    <InputGift
+                    onChange={(e) => setNameOfGift(e.target.value)}
+                    type="text"
+                    placeholder="Enter gift here..."
+                    />
+                    <InputPrice
+                    onChange={(e) => setTrackingID(Number(e.target.value))}
+                    type="number"
+                    placeholder="Enter tracking ID here..."
+                    />
+                    <InputPerson
+                    onChange={(e) => setForWhomGift(e.target.value)}
+                    type="text"
+                    placeholder="Enter gift for whom here..."
+                    />
+                    <InputLink
+                    type="text"
+                    placeholder="Enter delivery company here..."
+                    onChange={(e) => setDeliveryCompany(e.target.value)}
+                    />
+                    <AddToListButton
+                    onClick={async(e) => {
+                        // setting the main items useState
+                        e.preventDefault()
+                        let response = await fetch('https://gift-space.herokuapp.com/api/add-tracking/', {
+                            method:'POST',
+                            headers:{
+                                'Content-Type':'application/json',
+                                'Authorization':'Bearer ' + String(authTokens.access)
+                            },
+                            body:JSON.stringify({'company':company, 'trackingID':trackingID, 'description':description, 'recipient':recipient})
+                        })
+                        let data = await response.json()
+                        let id = data.id
+                        if(response.status === 200){
+                            setIDofGift(data.id)
+                        }
+                        else {
+                            alert('ERROR ADDING GIFT: ', data)
+                        }
+
+                        setItems((prev) => {
+                        return [
+                            ...prev,
+                            {id, description, trackingID, recipient, company},
+                        ];
+                        });
+                        setAddItem(!addItem);
+                        console.log('SET ITEMS: ', items)
+                    }}
+                    >
+                    Add
+                    </AddToListButton>
+                </AddItemPopUp>
+
+                <ActualList>
+                    {/* error info message if the total budget is falling short of the gift's total*/}
+
+
+                    <ListObject>
+                    {/* input the username of the user from the backend instead of shreyas */}
+                        <GiftName>{user.username}'s Gift List</GiftName>
+                        <GiftPrice> Tracking ID </GiftPrice>
+                        <ForWhom> Recipient </ForWhom>
+                        <LinkToGift>
+                            Company
+                        </LinkToGift>
+                    
+                    </ListObject>
+                    {items.map((e, id) => {
+                    return (
+                        <>
+                        <ListObject key={id}>
+                            {console.log('AFTER ITEMS: ', items)}
+                            {console.log('AFTER ITEMS E: ', e)}
+                            {console.log('AFTER ITEMS ID: ', id)}
+                            <GiftName>{e.description}</GiftName>
+                            <GiftPrice>{e.trackingID}</GiftPrice>
+                            <ForWhom>{e.recipient}</ForWhom>
+                            <LinkToGift>{e.company}</LinkToGift>
+                            <Delete key={id}
+                            id={id}
+                            onClick={ async() => {
+                                let response = await fetch(`https://gift-space.herokuapp.com/api/delete-gift/${e.id}/`, {
+                                    method:'POST',
+                                    headers:{
+                                        'Content-Type':'application/json',
+                                        'Authorization':'Bearer ' + String(authTokens.access)
+                                    }
+                                })
+                                let data = await response.json()
+
+                                if(response.status === 200){
+                                    alert(data)
+                                }
+                                else{
+                                    alert('ERROR DELETING GIFT: ', data)
+                                }
+                                let newArr = items.filter((id) => id.id !== e.id);
+                                setItems(newArr);
+                            }}
+                            title="Delete Item"
+                            />
+                        </ListObject>
+                        </>
+                    );
+                    })}
+
+                    <AddItemButton onClick={() => setAddItem(!addItem)}>
+                    Add a tracking data
+                    </AddItemButton>
+                </ActualList>
         </div>
     )
 }
